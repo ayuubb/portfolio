b1bdac3c5001f4652f4716fe977e198346277da5
ayuubb <ayubbkj2@gmail.com>
2023-03-23 22:59:35 +0700

    add data dummy

---
+++ src/Data.jsx
@@ -0,0 +1,42 @@
+import {
+    IconBrandHtml5,
+    IconBrandBootstrap,
+    IconBrandJavascript,
+    IconBrandSvelte,
+    IconBrandReact,
+    IconBrandNpm,
+    IconBrandGit,
+    IconBrandSass,
+} from '@tabler/icons-react';
+
+export const skillIcons = [
+    <IconBrandHtml5 size={'64'} stroke={1.5} />,
+    <IconBrandBootstrap size={'64'} stroke={1.5} />,
+    <IconBrandSvelte size={'64'} stroke={1.5} />,
+    <IconBrandReact size={'64'} stroke={1.5} />,
+    <IconBrandNpm size={'64'} stroke={1.5} />,
+    <IconBrandGit size={'64'} stroke={1.5} />,
+    <IconBrandJavascript size={'64'} stroke={1.5} />,
+    <IconBrandSass size={'64'} stroke={1.5} />,
+];
+
+export const experiences = [
+    {
+        id: 1,
+        year: '2022',
+        position: 'Frontend Web Developer',
+        company: 'merchant.id',
+    },
+    {
+        id: 2,
+        year: '2021',
+        position: 'Frontend Web Developer Student',
+        company: 'dicoding.com',
+    },
+    {
+        id: 3,
+        year: '2019',
+        position: 'Frontend web developer Intern',
+        company: 'PT. Inspirasi Mandiri Nusantara',
+    },
+];
--- src/sections/Experience.jsx
+++ src/sections/Experience.jsx
@@ -1,9 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Button from '../components/Button.jsx';
+import { experiences, skillIcons } from '../Data.jsx';
 
 const Experience = () => {
+    const [active, setActive] = useState(1);
     return (
         <section className={'pt-16 pb-8 lg:pt-24'}>
-            <div>Experience</div>
+            <div className={'text-center mb-8 lg:mb-14'}>
+                <span className={'text-sm mb-2'}>My abilities</span>
+                <h2 className={'text-xl text-gray-400'}>Skills And Experience</h2>
+            </div>
+            <div className={'flex gap-3 justify-center pb-8 lg:pb-16'}>
+                <Button onClick={() => setActive(1)} className={''} text={'Experiences'} />
+                <Button onClick={() => setActive(0)} className={''} text={'Skills'} />
+            </div>
+            {active === 1 ? (
+                <div className={'grid grid-cols-2 gap-4 lg:grid-cols-4 px-10 lg:px-48'}>
+                    {skillIcons.map((icon, index) => {
+                        return (
+                            <div className={'mx-auto p-3'} key={index}>
+                                {icon}
+                            </div>
+                        );
+                    })}
+                </div>
+            ) : (
+                <div className={'grid grid-cols-1 gap-4 px-6 lg:px-48'}>
+                    {experiences.map((experience, index) => {
+                        return (
+                            <div className={'grid grid-cols-2 gap-2 justify-between'} key={index}>
+                                <h4 className={'text-2xl lg:text-3xl text-center'}>{experience.year}</h4>
+                                <div className='flex flex-col justify-center'>
+                                    <span className={'text-left'}>{experience.position}</span>
+                                    <a className={'text-sm text-gray-400'} href='#'>
+                                        {experience.company}
+                                    </a>
+                                </div>
+                            </div>
+                        );
+                    })}
+                </div>
+            )}
         </section>
     );
 };
